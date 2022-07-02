5fbf34fe4f92372718bce12fed5072711f99b0d7
Alex T <alex@restivetech.com.au>
2022-07-02 10:40:43 +1000

    Ax fixes and updates

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,39 +1,15 @@
-import React, { useState} from "react";
-import InputField from "./InputField";
-import Print from "./TodoListItem";
+import React from "react";
+import { ToDoComponent } from "./to-do/ToDo";
+
+import { Container } from "@mui/material";
 
 const App = () => {
-
-  const [todoList, setTodoList] = useState<Array<TodoData>>([])
-
-  const toggleTodoItem = (selectedItem: TodoData) => {
-    setTodoList(todoList => todoList.map((item) => {
-      if (item.id === selectedItem.id) {
-        return { ...item, done: !item.done};
-      }
-        return item
-    }))
-   
-  };
-
-  const removeItem = (delItem: TodoData) => {
- 
-    setTodoList(todoList => todoList.filter((item) => item.id !== delItem.id));
-    
-  };
-
-  const addTodo: AddTodo = (newTodo) => {
-    setTodoList([...todoList, {text: newTodo, id: new Date().toISOString(), done: false}])
-  };
-
   return (
-    <div>
+    <Container maxWidth="md">
       <h1>Todo App</h1>
-      <InputField addTodo={addTodo} />
-      <br />
-      <Print todoList={todoList} toggleTodoItem={toggleTodoItem} removeItem={removeItem}/> 
-    </div>
+      <ToDoComponent />
+    </Container>
   );
-}
+};
 
-export default App
+export default App;
---
+++ src/components/to-do/ListItem.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+import DeleteForeverRoundedIcon from "@mui/icons-material/DeleteRounded";
+import IconButton from "@mui/material/IconButton";
+import { Stack } from "@mui/material";
+import { TodoData } from "./ToDo";
+
+const ListItem = ({
+  todoList,
+  toggleTodoItem,
+  removeItem,
+}: {
+  todoList: TodoData[];
+  toggleTodoItem: (event: TodoData) => void;
+  removeItem: (event: TodoData) => void;
+}) => {
+  return (
+    <div>
+      {todoList.map((item) => {
+        return (
+          <Stack
+            style={{ display: "flex", justifyContent: "space-between" }}
+            key={item.id}
+          >
+            <li
+              style={{ textDecoration: item.done ? "line-through" : undefined }}
+              onClick={() => toggleTodoItem(item)}
+            >
+              {item.text}
+              <IconButton type="button" onClick={() => removeItem(item)}>
+                <DeleteForeverRoundedIcon sx={{ color: "darkred" }} />
+              </IconButton>
+            </li>
+          </Stack>
+        );
+      })}
+    </div>
+  );
+};
+
+export default ListItem;
